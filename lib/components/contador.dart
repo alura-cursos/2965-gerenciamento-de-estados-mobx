7dbc684b16e81f21779833d3a1099ff0aa436192
ikyrie <mah.c5r@gmail.com>
2023-03-09 10:39:14 -0300

    Video 1.3

--- lib/components/contador.dart
+++ lib/components/contador.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
-class Contador extends StatelessWidget {
+class Contador extends StatefulWidget {
 Contador({ Key? key }) : super(key: key);
 
+  @override
+  State<Contador> createState() => _ContadorState();
+}
+
+class _ContadorState extends State<Contador> {
+  int valorContador = 0;
   @override
   Widget build(BuildContext context){
     return Row(
@@ -11,15 +17,22 @@ Contador({ Key? key }) : super(key: key);
         InkWell(
           borderRadius: BorderRadius.circular(20),
           onTap: () {
+            setState(() {
+              if(valorContador > 0) {
+                valorContador--;
+              }
+            });
               
           },
           child: const Icon(Icons.remove_circle_outline, size: 20,),
         ),
-        Text("0"),
+        Text(valorContador.toString()),
         InkWell(
           borderRadius: BorderRadius.circular(20),
           onTap: () {
-              
+             setState(() {
+               valorContador++;
+             }); 
           },
           child: const Icon(Icons.add_circle_outline, size: 20,),
         ),
